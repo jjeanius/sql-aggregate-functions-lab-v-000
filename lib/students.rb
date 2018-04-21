4257f326d1c7a5143898084d6fac3b167efb767d
Judy McGoldrick <jjeanius@verizon.net>
2018-04-21 03:28:02 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,6 +5,7 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
+  "select min(gpa) from students;"
 end
 
 def average_student_gpa
@@ -14,4 +15,5 @@ def total_tardies_for_all_students
 end
 
 def average_gpa_for_9th_grade
+
 end
